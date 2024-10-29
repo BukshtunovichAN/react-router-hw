756421cba11c13657030b98965bcd8b5b40943c6
Anastasiya Bukshtunovich <nastya.trader@gmail.com>
2024-10-29 17:42:49 +0300

    innit commit

--- src/index.js
+++ src/index.js
@@ -2,9 +2,10 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { App } from 'components/App';
 import './index.css';
+import { BrowserRouter } from 'react-router-dom';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
+  <BrowserRouter basename="/react-router-hw">
     <App />
-  </React.StrictMode>
+  </BrowserRouter>
 );
